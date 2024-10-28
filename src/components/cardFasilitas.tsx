16f40db5ee471343431a212fd733034c633f38da
saaip7 <syaifullahhilmimaarij2003@mail.ugm.ac.id>
2024-10-28 23:51:33 +0700

    fix: card facility responsive

--- src/components/cardFasilitas.tsx
+++ src/components/cardFasilitas.tsx
@@ -7,8 +7,8 @@ interface FacilityCardProps {
 
 const CardFasilitas: React.FC<FacilityCardProps> = ({ icon, title }) => {
   return (
-    <div className="flex flex-col rounded-none w-[193px]">
-      <div className="flex flex-col px-5 py-12 bg-white rounded-lg border border-gray-200 border-solid h-[193px] w-[193px] max-md:px-5">
+    <div className="flex flex-col rounded-none w-[160px] md:w-[193px]">
+      <div className="flex flex-col px-5 py-12 bg-white rounded-lg border border-gray-200 border-solid h-[160px] w-[160px] md:w-[193px] md:h-[193px] max-md:px-5">
         <div className="w-10 h-10 mx-auto text-color-red">{icon}</div>
         <div className="mt-4 text-center text-color-black font-semibold">{title}</div>
       </div>
